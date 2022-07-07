02801b3a28f6fa1b369e2f1ee74d888d58be7e94
Carlo Montangero <carlo@montangero.eu>
2022-07-07 16:36:55 +0200

    Agginuta trascrizione moderna

--- additional_materials/newItems/TerzaProva.lisp
+++ additional_materials/newItems/TerzaProva.lisp
@@ -1,3 +1,4 @@
+
 (putdq label 
      (lambda (x) (set x  (getap)))))
 
@@ -15,12 +16,31 @@
                       ((lambda (y) (print (cons y y)) (getap))
                        x)))
                (cond ((eq (quote a) (car x))
-                      (go a)
+                      (go a))
                      ((quote t)
-                      x))))))
+                      x))))
 
-Comportamento: fun mangia tutte le "a" nel suo primo (e unico fornito) argomento, dopo averne escluso, per ragioni misteriose, il primo elemento.
-               Ad ogni passo stampa due volte ciò che resta dell'argomento stesso. In questa prova, con argomento (b a b b),
-               stampa  ((A B B) A B B), ((B B) B B) e poi restituisce (B B). Sehr compliziert!
 Domanda: e' un  caso che la label si chiami "a", o e' necessario? Ossia, cambiando "a" in "l", tranne che a riga 17, cambia qualcosa?
+Commento: Il secondo argomento serve solo per introdurre un nome cui associare la label. Per cui direi che si può rinominare.
 Domanda: Cosa ci sta a fare il (getap) a linea 15? Per debug, visto che setdeb è attivo? Il valore viene perso...
+
+Riscritoo in stile più moderno, trascurando i vincoli imposti dalla natura primitiva dell'interprete del 1973, si ha :
+
+(putdq fun 
+     (lambda (x l) 
+          (label 'l)
+               (setq x (cdr x))
+               (print (cons x x))
+               (cond ((eq (car x) 'a) (go l))
+                     (t x))))
+
+da cui è piuù facile capire il comportamento : 
+     fun mangia tutte le "a" nel suo primo argomento, dopo averne escluso, per ragioni misteriose, il primo elemento.
+     Ad ogni passo stampa due volte ciò che resta dell'argomento stesso. In questa prova,
+     con argomento (b a b b) stampa  ((A B B) A B B), ((B B) B B) e infine restituisce (B B). Sehr compliziert!
+
+Caveat: label e go non sono le primitive della forma prog, non ancora disponibile nell'interprete del 1973, ma funzioni costruite
+     con le primitive definite dall'estensione MAGMA, il cui esatto funzionamento va ricostruito. In prima approssimazione
+     si può dire che label cattura il blocco di controllo (A-block) all'inizio di fun, e go lo usa per rilanciarne l'esecuzione,
+     tramite una funzione vuota (l'uso di (cons x) come lista di argomenti in apply è misterioso. Inoltre, pare quasi che l'interprete 
+     non riconoscesse ancora NIL, visto l'uso di (quote) per esprimerlo)
